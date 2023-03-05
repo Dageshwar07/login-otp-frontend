a0cbb677b31dbd6496f2eb3df045304bc0e76ad8
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-03-06 02:16:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Otp from './pages/Otp';
+import Error from './pages/Error';
+import Headers from './components/Header';
+import { Routes, Route } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Headers />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/user/otp' element={<Otp />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </>
   );
 }
 
